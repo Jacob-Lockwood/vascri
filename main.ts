cc8b1bce3be3f92204650c30f701d72a01ad05ad
Jacob <jacobulysseslockwood@gmail.com>
2024-06-19 13:31:05 -0400

    initial commit

---
+++ main.ts
@@ -0,0 +1,49 @@
+import { readAll } from "@std/io/read-all";
+let code = new TextDecoder().decode(await readAll(Deno.stdin));
+
+const or = (fns: readonly (() => string)[]) => {
+  for (const fn of fns) {
+    try {
+      return fn();
+    } catch {
+      continue;
+    }
+  }
+  throw new Error("or failed");
+};
+function* many(fn: () => string) {
+  while (code.length) {
+    try {
+      yield fn();
+    } catch (_) {
+      break;
+    }
+  }
+}
+const optional = (chr: string) => t(new RegExp(`^\\${chr}?`)) || chr;
+const t = (re: RegExp) => {
+  code = code.slice(code.search(/[^ ]/));
+  const match = code.match(re)![0]; // throws if no match
+  code = code.slice(match.length);
+  return match;
+};
+const num = () => `new $N(${Number(t(/^[+-]?(?:\d*\.\d+|\d+)/))})`;
+const str = () => `new $S(${t(/^"(?:\\.|[^"])*"/)})`;
+const chr = () => `new $S('${t(/^'./)[1]}')`;
+const nme = () => t(/^[A-Z]/);
+const arr = () =>
+  t(/^\[/) + [...many(() => expr() + optional(","))].join("") + optional("]");
+const nilad = () => or([arr, num, str, chr, nme]);
+const triad = () => `.${t(/^[a-z][\u0324:]/)[0]}(${arg()},${arg()})`;
+const dyad = () => `.${t(/^[a-z][\u0323\.]/)[0]}(${arg()})`;
+const monad = () => `.${t(/^[a-z]/)}()`;
+const infix = () => t(/^[+\-*/]/) + arg();
+const chain = () => or([triad, dyad, monad, infix]);
+function arg(): string {
+  const z = () => or([nilad, () => "X" + chain()]);
+  const p = () => t(/^\(/) + expr() + optional(")");
+  return or([p, z]);
+}
+function expr() {
+  return arg() + [...many(chain)].join("");
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Vascri
+
+(v0.0.-1)
+
+**Note**: Vascri is in a very early stage of development. No functions have been
+implemented yet, only a rudimentary parser, and that too is still missing
+certain features. This notice will be removed when it hits v0.1.0.
+
+Vascri is a code-golf language designed to be a spiritual successor to
+ETHProduction's language [Japt]. Japt is a shortened version of JavaScript, and
+Vascri is a shortened version of Japt. (Admittedly, the difference between the
+length of JavaScript and Japt programs is far greater than the length difference
+between Japt and Vascri programs, but that's just how this goes.)
+
+Japt's name comes from taking the first and last two characters of JavaScript:
+
+<!-- deno-fmt-ignore -->
+<u>**Ja**</u>vascri<u>**pt**</u>
+
+Therefore, Vascri takes the rest:
+
+<!-- deno-fmt-ignore -->
+Ja<u>**vascri**</u>pt
+
+## Motivation
+
+Japt was a very efficient code-golf language for its time, but in recent years
+it has struggled to keep up with newer languages such as [Vyxal]. On
+[Code Golf Stack Exchange's language leaderboard](https://codegolf.meta.stackexchange.com/a/8891/108687),
+we can see that while Japt was at 4th place for shortest language in 2016, by
+2024 it has fallen down to 16th place.
+
+If we look at Japt code side-by-side with Vyxal, we can see some glaring issues
+that make Japt code longer.
+
+[Japt]: https://github.com/ETHproductions/japt
+[Vyxal]: https://github.com/Vyxal/Vyxal
